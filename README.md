ccdd49d95d0ee6f0405eb69b17e977188e49a814
Chloe Harris <chloe.a.harris17@gmail.com>
2022-07-08 16:43:55 -0700

    updated UI, cleaned up html code, updated README, working on API requests

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# Weather Dashboard
+# Weather Dashboard ![favicon](./cloudy.png)
+
+![license badge](https://img.shields.io/badge/license-MIT-blue)
 
 ## Description
 
-A weather dashboard that allows users to search for current and future weather information by city.  
+A weather dashboard application that allows users to search for current and future weather information by city.  
 
-This application uses the OpenWeather One Call API to retreive weather data and local storage to save persistent data. 
+This application uses the OpenWeather One Call API to retreive weather data and local storage to save persistent data with dynamically updated HTML and CSS.
+
+## Features 
+
+* jQuery
+* Moment.js
+* Bootstrap
+* Server-Side API [OpenWeather](https://openweathermap.org/)
 
 
 ## Mock-Up
@@ -12,8 +21,15 @@ This application uses the OpenWeather One Call API to retreive weather data and 
 The following image shows the web application's appearance and functionality:
 ![WeatherDashboard](https://user-images.githubusercontent.com/89039793/135965971-091d6adb-c0af-4dec-b6b2-77f85b5cb533.JPG)
 
-## Deployed Application
+## Link
 https://chloeharris1.github.io/weatherdashboard/
+
+## License
+Licensed under the MIT license.
+
+## Credits 
+
+Weather favicon from [iconixar](https://www.flaticon.com/authors/iconixar)
 
 ## Questions
 [Email Me](Chloe.a.harris17@gmail.com) if you have any questions.
--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function getCity() {
         // Query a stored value
         var historyCity = localStorage.getItem(i);
         // console.log(city);
-        var cityName = $(".list-group").addClass("list-group-item");
+        var cityName = $(".list-group").addClass("list-group-item list-group-item-action list-group-item-secondary text-center rounded");
         // Add user search history in list below search field 
         cityName.append("<li>" + city + "</li>");
     }
@@ -36,7 +36,7 @@ function getWeather(cityName) {
 
     fetch(queryURL)
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(function(data) {
@@ -46,26 +46,46 @@ function getWeather(cityName) {
             var template = `
                 <h1 class="d-inline" id="current-city">${data.name}</h1>
                 <img class="d-inline" id="city-name-icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"
-        alt="weather icon">
-                <p>Temperature:<span class="current" id="temperature">${data.main.temp}</span></p>
-                <p>Humidity:<span class="current" id="humidity">${data.main.humidity}</span></p>
-                <p>Wind Speed:<span class="current" id="wind speed">${data.wind.speed}</span></p>
+                alt="weather icon">
+                <p>Temperature:<span class="current" id="temperature"> ${data.main.temp}</span></p>
+                <p>Humidity:<span class="current" id="humidity"> ${data.main.humidity}</span></p>
+                <p>Wind Speed:<span class="current" id="wind speed"> ${data.wind.speed}</span></p>
                 <p>UV Index:<span class="current bg-danger rounded py-2 px-2 text-white"
                     id="uv-index"></span></p>
             `;
-
+            $('#weatherData').css('display', 'block')
             document.querySelector("#current-weather").innerHTML = template;
 
         });
+
+// Retrieve UV Index data from API 
+    // var lat = coord.lat; 
+    // var lon = coord.lon;
+    // var uviQuery =  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}`
+    // console.log(uviQuery)
+
+    // fetch(uviQuery)
+    //     .then(function (response) {
+    //         console.log(response);
+    //         return response.json();
+    //     })
+    //     .then(function(data) {
+    //         console.log("uvi", data);
+        //     var template = `<p>UV Index: 
+        //     <span id="uvIndexColor" class="px-2 py-2 rounded">${data.main}</span>
+        // </p>`
+        // })
+
+
 }
 
 // Retrieve 5-day forecast data from API
 function forecast(cityName) {
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
-    console.log(queryURL);
+    // console.log(queryURL);
     fetch(queryURL)
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(function(data) {
@@ -83,7 +103,7 @@ function forecast(cityName) {
             list.forEach(function(item) {
                 // HTML template for adding dynamically generated forecast data to page
                 template += `
-                    <div class="col-sm2 forecast text-white ml-2 mb-3 p-2 mt-2">
+                    <div class="col-sm-2 forecast text-white m-2 mb-3 p-2 mt-2">
                         <div class="card bg-dark text-light">
                             <div class="card-body">
                                 <h5 class="card-title-date" id="day1-date">${new Date(item.dt_txt).toLocaleDateString()}</h5>
